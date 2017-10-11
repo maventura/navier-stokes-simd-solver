47b30353e73689dcfc64761fbf08c9f02982446f
lauramuino <lauramuino2@gmail.com>
2017-10-10 22:50:33 -0300

    Makefile with compiling command for assembly version. Adding info about aligning the stack and registers to preserve.

--- Makefile
+++ Makefile
@@ -3,13 +3,20 @@
 
 .PHONY: clean
 
-main : main.cpp mat3.h simulator.h io.h
+main: main.cpp mat3.h simulator.h io.h
 	g++ -std=c++11 main.cpp -o main
 
 clean:
 	rm -f main
+	rm -f assmain
 	rm ./out/*
 
 help: 
 	@echo 'For principal cpp executable, run make.'
-	@echo 'That is all for now'
+	@echo 'For compiling the assembly version, run make assembly.'
+	@echo 'To clean the executable file, run clean.'
+	@echo 'That is all for now...'
+
+assembly: main.cpp vvp_asm.asm mat3.h simulator.h io.h
+	nasm -f elf64 vvp_asm.asm -o vvp_asm.o
+	g++ -o assmain -std=c++11 main.cpp vvp_asm.o
--- vvp_asm.asm
+++ vvp_asm.asm
@@ -11,90 +11,102 @@ vvp_asm:
 
 	push rbp
 	mov rbp, rsp
-	push r8
-	push r9
-	push r10
+  push r15  ;estos son los 5 registros a preservar... si es que los pisamos
+  push r14
+  push r13
+  push r12
+  push rbx
+  sub rsp, 8  ;la pila debe estar alineada a 16 bits
 
-	;chequear si hace falta asegurar los xmmi
+  ;int vvp_asm(
+    ;float *data,     rdi
+    ;int pos          rsi
+  ;)
+
+
+
+
 	;en xmm0 esta el offset
 	;en  rdi el puntero a la matriz
 
-	movd xmm15, 0.8
-	mov xmm10, ; r = dt / (Re * h * h)
+	; movd xmm15, 0.8
+	; mov xmm10, ; r = dt / (Re * h * h)
 
 
-	mov xmm14, delta
-	xor xmm1, xmm1
-    ;xmm10,  r (FIJO) pasar a xmm15
-    double p1 = (-U2.at(i + 1, j, k) + r);  //xmm1 //res/acum p_i/omx2(i, j, k)
-											;xor xmm2, xmm2    									
-    double p2 = (-V2.at(i, j + 1, k) + r)	;sub xmm2, p_i (las primeras dos veces)
-    										;add xmm2, p_i (las 4 veces restantes)
-                                            ;add xmm2, xmm10(r)
-    double p3 = (U2.at(i - 1, j, k) + r);   //mov xmm3, om_i
-    double p4 = (V2.at(i, j - 1, k) + r);   //mul xmm2, xmm3
-    double p5 = (W2.at(i, j, k + 1) + r);   //add xmm1, xmm2
-    double p6 = (W2.at(i, j, k - 1) + r);   //"c" = xmm internal index.
-                                            //no tocar xmm1
+	; mov xmm14, delta
+	; xor xmm1, xmm1
+ ;    ;xmm10,  r (FIJO) pasar a xmm15
+	; ;xmm1 //res/acum p_i/omx2(i, j, k)    	double p1 = (-U2.at(i + 1, j, k) + r);  
+	; xor xmm2, xmm2    																					
+	; sub xmm2, p_i ;(las primeras dos veces)    	double p2 = (-V2.at(i, j + 1, k) + r)	
+	; add xmm2, p_i ;(las 4 veces restantes)	    										
+	; add xmm2, xmm10(r)	;                                            
+	; mov xmm3, om_i    ;	double p3 = (U2.at(i - 1, j, k) + r);   
+	; mul xmm2, xmm3    ;	double p4 = (V2.at(i, j - 1, k) + r);   
+	; add xmm1, xmm2   ; 	double p5 = (W2.at(i, j, k + 1) + r);   
+	; ;"c" = xmm internal index.    	double p6 = (W2.at(i, j, k - 1) + r);   
+ ;                                            ;no tocar xmm1
 
-                                                        //xor xmm2, xmm2 // axs/acum ax_i/res
-    double ax1 = omy2.at(i, j, k) * U2.at(i, j + 1, k); //mov xmm3, omy3
-                                                        //mov xmm4, U2
-                                                        //mul xmm3, xmm4
-                                                        //add xmm2, xmm3
-    double ax2 = -omy2.at(i, j, k) * U2.at(i, j - 1, k);//same
-    double ax3 = omz2.at(i, j, k) * U2.at(i, j, k + 1);//same
-    double ax4 = -omz2.at(i, j, k) * U2.at(i, j, k - 1);//same
+	; xor xmm2, xmm2 ; axs/acum ax_i/res
+    
+ ;    mov xmm3, omy3
+	; mov xmm4, U2
+	; mul xmm3, xmm4
+	; add xmm2, xmm3 ;double ax1 = omy2.at(i, j, k) * U2.at(i, j + 1, k); 
+ ;    ;double ax2 = -omy2.at(i, j, k) * U2.at(i, j - 1, k);//same
+ ;    ;double ax3 = omz2.at(i, j, k) * U2.at(i, j, k + 1);//same
+ ;    ;double ax4 = -omz2.at(i, j, k) * U2.at(i, j, k - 1);//same
 
-    double axs = ax1 + ax2 + ax3 + ax4;
+ ;    ;double axs = ax1 + ax2 + ax3 + ax4;
 
-    omx2.set(i, j, k, p1 * omx2.at(i + 1, j, k) + p2 * omx2.at(i, j + 1, k)//xmm1
-             + p3 * omx2.at(i - 1, j, k) + p4 * omx2.at(i, j - 1, k)//xmm1
-             + p5 * omx2.at(i, j, k + 1) + p6 * omx2.at(i, j, k - 1)//xmm1
-             + (1.0 / q)*omx1.at(i, j, k)   //mov xmm15, 1/q (FIJO)
-                                            //mov xmm3, omx1
-                                            //mul xmm3, xmm15
-             + axs);//xmm2
+ ;    ;omx2.set(i, j, k, p1 * omx2.at(i + 1, j, k) + p2 * omx2.at(i, j + 1, k)//xmm1
+ ;             + p3 * omx2.at(i - 1, j, k) + p4 * omx2.at(i, j - 1, k)//xmm1
+ ;             + p5 * omx2.at(i, j, k + 1) + p6 * omx2.at(i, j, k - 1)//xmm1
+ ;             + (1.0 / q)*omx1.at(i, j, k)   
+ ;    mov xmm15, 1/q ;(FIJO)
+ ;    mov xmm3, omx1
+ ;    mul xmm3, xmm15
+ ;             ;+ axs);//xmm2
 
     
-    omx2.set(i, j, k, omx2.at(i, j, k) * (q / delta)); //add xmm1, xmm2
-                                                        //add xmm1, xmm3    //  xmm1 = set. (xmm2/3 libres)
-                                                        //div xmm1, xmm15   //  /= delta
-                                                        //div xmm1, xmm14   //  *=(1/q)
+ ;    omx2.set(i, j, k, omx2.at(i, j, k) * (q / delta)); //add xmm1, xmm2
+ ;                                                        //add xmm1, xmm3    //  xmm1 = set. (xmm2/3 libres)
+ ;                                                        //div xmm1, xmm15   //  /= delta
+ ;                                                        //div xmm1, xmm14   //  *=(1/q)
 
-        omx2.set(i, j, k, (1.0 - wt)*aux_omx2 + wt * omx2.at(i, j, k));//mov xmm13, wt (=0.8 FIJO)
-                                                                      //mul xmm1, xmm13
-                                                                      //mov xmm2, 0x0001
-                                                                      //sub xmm2, xmm13
-                                                                      //mov xmm3, omx2(i,j,k)
-                                                                      //mul xmm3, xmm2
-                                                                      //add xmm1, xmm3 
+ ;        omx2.set(i, j, k, (1.0 - wt)*aux_omx2 + wt * omx2.at(i, j, k));//mov xmm13, wt (=0.8 FIJO)
+ ;                                                                      //mul xmm1, xmm13
+ ;                                                                      //mov xmm2, 0x0001
+ ;                                                                      //sub xmm2, xmm13
+ ;                                                                      //mov xmm3, omx2(i,j,k)
+ ;                                                                      //mul xmm3, xmm2
+ ;                                                                      //add xmm1, xmm3 
 
-                                                            //mov [omx2], xmm1
+ ;                                                            //mov [omx2], xmm1
 
 
-    //Eliptica eje x.
-    //p1=p2=p3=p4=p5=p6=1/6.0;                                    
-    psix2.set(i, j, k, (psix2.at(i + 1, j, k) + psix2.at(i, j + 1, k)   //mov xmm1, psix2 (levanto uno, una vez)
-                        + psix2.at(i - 1, j, k) + psix2.at(i, j - 1, k) //mov xmm2, pxix2 (levanto otro, aca itera)
-                        + psix2.at(i, j, k + 1) + psix2.at(i, j, k - 1) //add xmm1, xmm2 (sumo y vuelvo arriba)
-                        + h * h * omx2.at(i, j, k)) / 6.0);             // mov xmm12, h (DEPENDE PARAMS DE ENTRADA, pero nunca cambia)
+ ;    //Eliptica eje x.
+ ;    //p1=p2=p3=p4=p5=p6=1/6.0;                                    
+ ;    psix2.set(i, j, k, (psix2.at(i + 1, j, k) + psix2.at(i, j + 1, k)   //mov xmm1, psix2 (levanto uno, una vez)
+ ;                        + psix2.at(i - 1, j, k) + psix2.at(i, j - 1, k) //mov xmm2, pxix2 (levanto otro, aca itera)
+ ;                        + psix2.at(i, j, k + 1) + psix2.at(i, j, k - 1) //add xmm1, xmm2 (sumo y vuelvo arriba)
+ ;                        + h * h * omx2.at(i, j, k)) / 6.0);             // mov xmm12, h (DEPENDE PARAMS DE ENTRADA, pero nunca cambia)
 
-                                                                        //mov xmm2, xmm12
-                                                                        //mul xmm2, xmmm12
-                                                                        //mov xmm3, omx2
-                                                                        //mul xmm2, xmm3
-                                                                        //div xmm2, xmm11 (=6.0)
-                                                                        //add xmm1, xmm2
+	; mov xmm2, xmm12
+	; mul xmm2, xmmm12
+	; mov xmm3, omx2
+	; mul xmm2, xmm3
+	; div xmm2, xmm11 (=6.0)
+	; add xmm1, xmm2
 
-    psix2.set(i, j, k, (1.0 - wt)*aux_psix2 + wt * psix2.at(i, j, k));  //mul xmm1, xmm13
-                                                                      //mov xmm2, 0x0001
-                                                                      //sub xmm2, xmm13
-                                                                      //mov xmm3, psix2(i,j,k)
-                                                                      //mul xmm3, xmm2
-                                                                      //add xmm1, xmm3 
+ ;    psix2.set(i, j, k, (1.0 - wt)*aux_psix2 + wt * psix2.at(i, j, k));  //mul xmm1, xmm13
+ ;                                                                      //mov xmm2, 0x0001
+ ;                                                                      //sub xmm2, xmm13
+ ;                                                                      //mov xmm3, psix2(i,j,k)
+ ;                                                                      //mul xmm3, xmm2
+ ;                                                                      //add xmm1, xmm3 
 
-                                                                     //mov [psix2], xmm1
+ ;                                                                     //mov [psix2], xmm1
 
 
 
@@ -116,8 +128,12 @@ vvp_asm:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 mov rax, rdi
-pop r10
-pop r9
-pop r8
+
+add rsp, 8
+pop rbx
+pop r12
+pop r13
+pop r14
+pop r15
 pop rbp
 ret
