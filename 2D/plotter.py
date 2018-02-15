f9716840e0068868a5a3151837339225b7784064
Alejandro Martin Ventura <venturamartin90@gmail.com>
2018-02-14 23:38:44 -0300

    plotter

--- 2D/plotter.py
+++ 2D/plotter.py
@@ -4,6 +4,7 @@ import os
 import sys
 import math
 
+
 spacer = '   '
 print(os.linesep*2)
 print(spacer + '____       __  ___ ___ ____ ____ ')
@@ -12,14 +13,13 @@ print(spacer + '|    |___ |__|  |   |  |___ |  \ ')
 print(os.linesep*2)
 
 #Define parameters.
-param_file_name = "parameters.txt"
 min_side_size = 1
 max_side_size = 17
 min_time = 2
 max_time = 2
 version_list = ['asm', 'cpp', 'asm_omp', 'cpp_omp', 'icc']
 version = version_list[3]
-test_folder = '/home/martin/Desktop/repos/navier-stokes-simd-solver/2D/tests/'
+test_folder = '/home/martin/Desktop/repos/navier-stokes-simd-solver/2D/tests/cpp_omp_times_no_output_20/'
 total_tests = (max_time - min_time +1)*(max_side_size - min_side_size +1)
 
 
@@ -60,3 +60,19 @@ print(spacer + 'Variances: ' + os.linesep + spacer + str(variances))
 print(spacer + 'Stardard deviations: ' + os.linesep + spacer + str(std_devs))
 
 
+import matplotlib.pyplot as plt
+import numpy
+import seaborn as sns; sns.set(color_codes=True)
+
+
+
+
+
+data_array=numpy.array([numpy.array(xi) for xi in data])
+print(data_array)
+
+
+ax = sns.tsplot(data=data_array.transpose(), ci=[100])
+
+
+plt.show()
