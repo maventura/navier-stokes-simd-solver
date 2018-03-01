e8b921cd8b74a60b170ebfc4ff11cd7e468a58f0
Alejandro Martin Ventura <venturamartin90@gmail.com>
2018-03-01 04:21:19 -0300

    modified the plotter and added plots

--- 2D/plotter.py
+++ 2D/plotter.py
@@ -6,7 +6,7 @@ import math
 import matplotlib.pyplot as plt
 import numpy
 import seaborn as sns; sns.set(color_codes=True)
-
+from collections import OrderedDict
 
 
 
@@ -18,24 +18,53 @@ print(spacer + '|    |___ |__|  |   |  |___ |  \ ')
 print(os.linesep*2)
 
 #Define parameters.
-min_side_size = 5
-max_side_size = 6
+min_side_size = 1
+max_side_size = 20
 min_time = 2
 max_time = 2
 
+#Executable names:
 version_list_asm = ['asm','asm_o3','asm_ofast']
 version_list_cpp = ['cpp','cpp_o1','cpp_o2','cpp_o3','cpp_ofast']
 version_list_icc = ['icc', 'icc_o1', 'icc_o2', 'icc_o3', 'icc_ofast']
 version_list_omp = ['cpp', 'cpp_omp', 'cpp_omp_o1', 'cpp_omp_o2', 'cpp_omp_o3', 'cpp_omp_ofast']
+version_list_ofast = ['asm_ofast','cpp_ofast','icc_ofast','cpp_omp_ofast']
+version_list_all = version_list_asm+version_list_cpp+version_list_icc+version_list_omp
+
+#Corresponding plot labels:
+version_names_list_asm = ['Assembler (NASM) and GCC C++','Assembler (NASM) and GCC C++ -O3','Assembler (NASM) and GCC C++ -Ofast']
+version_names_list_cpp = ['GCC C++','GCC C++ -O1','GCC C++ -O2','GCC C++ -O3','GCC C++ -Ofast']
+version_names_list_icc = ['Intel C++ Compiler', 'Intel C++ Compiler -O1', 'Intel C++ Compiler -O2', 'Intel C++ Compiler -O3', 'Intel C++ Compiler -Ofast']
+version_names_list_omp = ['GCC C++', 'GCC C++ OpenMP', 'GCC C++ OpenMP -O1', 'GCC C++ OpenMP -O2', 'GCC C++ OpenMP -O3', 'GCC C++ OpenMP -Ofast']
+version_names_list_ofast = ['Assembler (NASM) and GCC C++ -Ofast', 'GCC C++ -Ofast', 'Intel C++ Compiler -Ofast', 'GCC C++ OpenMP -Ofast']
+version_names_list_all = version_names_list_asm+version_names_list_cpp+version_names_list_icc+version_names_list_omp
+
+#delete repeted elements from the 'all' lists preserving order
+version_list_all = list(OrderedDict.fromkeys(version_list_all))
+version_names_list_all = list(OrderedDict.fromkeys(version_names_list_all))
+
+
+
 
 #version_list = version_list_asm
-version_list = version_list_cpp
-#version_list = version_list_icc
-#version_list = version_list_omp
+#version_names_list = version_names_list_asm
 
+#version_list = version_list_cpp
+#version_names_list = version_names_list_cpp
+
+#version_list = version_list_icc
+#version_names_list = version_names_list_icc
+
+#version_list = version_list_omp
+#version_names_list = version_names_list_omp
+
+#version_list = version_list_ofast
+#version_names_list = version_names_list_ofast
+
+version_list = version_list_all
+version_names_list = version_names_list_all
 
 test_folder = '/home/martin/Desktop/repos/navier-stokes-simd-solver/2D/tests/to_plot/'
-
 
 
 
@@ -87,21 +116,29 @@ def read_as_array(min_time, max_time, min_side_size, max_side_size, version):
 	data_array=(numpy.array([numpy.array(xi) for xi in data])).transpose()
 	return data_array
 
+#plt.switch_backend('GTK')
+
+#[u'pgf', u'ps', u'Qt4Agg', u'GTK', u'GTKAgg',
+#u'nbAgg', u'agg', u'cairo', u'MacOSX', u'GTKCairo',
+#u'Qt5Agg', u'template', u'WXAgg', u'TkAgg', u'GTK3Cairo',
+#u'GTK3Agg', u'svg', u'WebAgg', u'pdf', u'gdk', u'WX']
+
+
+
+sns.set_context(font_scale=0.5, rc={"lines.linewidth": 1})
 
 for i in xrange(0,len(version_list)):
-	result_asm = read_as_array(min_time, max_time, min_side_size, max_side_size, version_list[i])
-	ax = sns.tsplot(data=result_asm, ci='sd',  color=sns.color_palette()[i%len(sns.color_palette())], condition = version_list[i])
+	result = read_as_array(min_time, max_time, min_side_size, max_side_size, version_list[i])
+	ax = sns.tsplot(data=result, ci='sd',  color=sns.color_palette()[i%len(sns.color_palette())], condition = version_names_list[i])
 
-#result_cpp = read_as_array(min_time, max_time, min_side_size, max_side_size, version_list[1])
-#result_cpp_omp = read_as_array(min_time, max_time, min_side_size, max_side_size, version_list[3])
+plt.savefig('foo.png', dpi=900, pad_inches=0)
 
-
-#ax2 = sns.tsplot(data=result_cpp, ci='sd', color='green', condition = 'C++')
-#ax3 = sns.tsplot(data=result_cpp_omp, ci='sd', color='blue', condition = 'OpenMP')
+manager = plt.get_current_fig_manager()
 
 
-
-plt.savefig('foo.png', dpi=900)
-
+plt.savefig('foo.png',  bbox_inches='tight')
+#Warning: figure gets saved with a smaller frame due to a bug of matplotlib. 
+#save the figure manualy from the plot window to get a better label positioning.
 
 plt.show()
+
